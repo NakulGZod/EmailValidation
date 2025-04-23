a653f2677f7f1135c4b1ae2e584239c6c6a717f4
NakulGZod <151714160+NakulGZod@users.noreply.github.com>
2025-04-23 21:54:46 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -21,24 +21,44 @@ let result = {
 }
 
 submitBtn.addEventListener("click", async (e) => {
-    e.preventDefault() 
-    console.log("Clicked!!")
-    resultCont.innerHTML = ` <img src="img/loading.svg" alt="" width="60">`
-    // Data Fetching
-    key = "ema_live_cC3PJZVEDDm0FcEq1hcqfPJIgfM3OuqVZMwWWNej"
-    let email = document.getElementById("email").value
-    
-    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    e.preventDefault();
+    console.log("Clicked!!");
+    resultCont.innerHTML = `<img src="img/loading.svg" alt="" width="60">`;
 
+    let key = "ema_live_cC3PJZVEDDm0FcEq1hcqfPJIgfM3OuqVZMwWWNej";
+    let email = document.getElementById("email").value;
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`;
 
-    let res = await fetch(url)
-    let result = await res.json()
-    let str = ``
+    let res = await fetch(url);
+    let result = await res.json();
 
-    for (key of Object.keys(result)) {
-        str = str + `<div>${key} : ${result[key]}</div>`
+    let validity = result["format_valid"] && result["smtp_check"] && result["state"] === "deliverable";
+
+    // Main result message
+    let statusMsg = validity
+        ? `<h3 style="color: green;">✅ Email is Valid</h3>`
+        : `<h3 style="color: red;">❌ Email is Invalid</h3>`;
+
+    // Details toggle button and hidden section
+    let detailsBtn = `<button class="btn" onclick="toggleDetails()">More Details</button>`;
+    let str = `<div id="details" style="display:none; margin-top:10px;">`;
+
+    for (let key of Object.keys(result)) {
+        str += `<div><b>${key}</b> : ${result[key]}</div>`;
     }
-    console.log(str)
 
-    resultCont.innerHTML = str
-})
+    str += `</div>`;
+
+    resultCont.innerHTML = statusMsg + detailsBtn + str;
+});
+
+// Function to show/hide details
+function toggleDetails() {
+    let details = document.getElementById("details");
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
